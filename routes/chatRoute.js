19ae83f61bb4f524ebea50ce00133b15ed829420
krishnapriya U.A <krishnapriyaua@gmail.com>
2025-05-22 14:58:50 +0530

    debugging

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -8,6 +8,7 @@ const VoicebotAPI = process.env.OPENROUTER_API
 router.post('/chat',async(req,res)=>{
     const message = req.body.message
     try {
+        console.log(message,'MESSAGE')
         const apiresponse = await fetch("https://openrouter.ai/api/v1/chat/completions",{
             method:'POST',
             headers:{
@@ -17,11 +18,15 @@ router.post('/chat',async(req,res)=>{
             body : JSON.stringify({
                 model:'mistralai/devstral-small:free',
                 messages:[{role:"user",content:message}]
-            })
+        })
+
+         
     })
+    console.log(apiresponse,'APIRESPONSE')
     const data = await apiresponse.json()
     
     const reply = data.choices?.[0]?.message?.content || 'Sorry no response for this question'
+     console.log(reply,'RePLY')
     
     res.json({reply})
 
